17c0df1ca8ee646ce4513d22c2daf0f25a0ecced
Yogesh9643 <rajputyogesh563@gmail.com>
2020-11-12 10:58:23 +0530

    added web3 to connect to metamask

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,35 @@ import Main from './Main'
 
 class App extends Component {
 
+  async componentWillMount(){
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+
+  async loadWeb3(){
+    if(window.ethereum){
+      window.web3= new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if(window.web3){
+      window.web3= new Web3(window.web3.currentProvider)
+    }
+
+    else{
+      window.alert('Non-Ethereum browser detected. You should consider trying metamask');
+    }
+  }
+
+  async loadBlockchainData(){
+    const web3= window.web3
+    const accounts= await web3.eth.getAccounts()
+    console.log(accounts)
+
+
+
+  }
+
   constructor(props) {
     super(props)
     this.state = {
@@ -26,7 +55,7 @@ class App extends Component {
             // Code...
             />
           }
-        }
+        )
       </div>
     );
   }
